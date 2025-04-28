9dab5608df4beaa8db8065a4c7e7c444bac42fff
jeihunmammadzada <jeihun.mammadzada@gmail.com>
2025-04-29 00:41:31 +0400

    Commited

--- out/_next/static/chunks/pages/components/Loading-08136cd4b6a47175.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7679],{2478:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/Loading",function(){return u(8950)}])}},function(n){n.O(0,[2888,9774,179],function(){return n(n.s=2478)}),_N_E=n.O()}]);
